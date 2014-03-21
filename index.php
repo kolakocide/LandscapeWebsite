bb653230a31be6e2844f7b224df43594874a6eaf
kolakocide <zachyo4545@gmail.com>
2014-03-21 12:41:22 -0400

    3/21/14
    This work is currently still in progress.

---
+++ index.php
@@ -0,0 +1,65 @@
+<html>
+	<head><title>York County School of Technology | Landscaping</title><link rel="shortcut icon" href="Icons/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+		<body background="Pics/Landscaping.jpg">
+		<font size="6">
+				<center><b>Welcome to the <u>York County School of Technology</u>
+<br>
+				Landscaping website!</center></b></font>
+<hr size="2" width="25%" align="center">
+			<center>
+				<h2><u>Instructor</u>:</h2>
+		<font color="lightblue" size="5">
+				<i><pre>Melissa Trocheck</pre></i>
+		</font>
+		
+		<div id="home"><p class="button"><a href="#null">Click this button!</a></p>
+		</div>
+		<font color="black" size="4">
+		</center>
+<br>
+				<div style="text-align:center;border:2px solid #00CCFF;width: 275px;position:absolute;top:215px;left:5px">
+					<center><h2><u>Areas of Study:</h2></u></center>
+					<ul style="display:inline-block;text-align:left">
+						<li>Pesticide Management</li>
+						<li>Small and Large Landscape Equipment Usage</li>
+						<li>Plant Identification</li>
+						<li>Plant and Soil Science</li>
+						<li>Landscape Maintenance</li>
+						<li>Greenhouse/Nursery Production</li>
+						<li>Turfgrass Management</li>
+						<li>Landscape Design and Installation</li>
+					</ul>
+				</div>
+				<center><div style="text-align:center;border:2px solid #00CCFF;width: 275px;position:absolute;top:215px;right:5px">
+					<center><h2><u>Educational and Work Background:</h2></u></center>
+					<ul style="display:inline-block;text-align:left">
+						<li>Bachelor in Landscape Architecture from the University of Maryland in 1994</li>
+						<li>International Society of Arboriculture Certified Arborist 1999-2006</li>
+						<li>Pesticide Sprayer Certification for Lawns and Trees and Shrubs 1999-2006</li>
+						<li>11+ years of work experience in the industry from retail sales to designing to installation</li>
+						<li>Currently teach the Residential Landscape Design 1 and 2 classes at Harrisburg Community College</li>
+					</ul>
+				</center></div>
+				
+				<center><div style="text-align:center;border:2px solid #00CCFF;width:275px;position:absolute;top:278px;left:650px">
+					<center><h2><u>Links:</h2></u></center>
+					<ul style="display:inline-block;text-align:left">
+						<li><a href="http://www.ycstech.org/education/district/district.php?sectionid=1" target="new">Ycstech Home Page</a></li>
+						<li><a href="http://www.ycstech.org/education/staff/staff.php?sectionid=124" target="new">Ycstech Staff Page</a></li>
+						<li><a href="http://www.ycstech.org/education/components/scrapbook/default.php?sectiondetailid=3943&" target="new">Landscaping Department Page</a></li>
+					</ul>
+				</center></div>
+		</font>
+					
+			<div class="video-holder" style="position:absolute;bottom:15px;left:500px">&nbsp;<iframe src="http://player.vimeo.com/video/69093499" width="561" height="305" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></p>
+			</div>
+			
+			
+			
+			
+					<center><font size="2px"><text style="position:absolute;bottom:15px;right:5px">&copy; 2005 - 2014 <a href="http://www.ycstech.org/education/district/district.php?sectionid=1" target="new"> York County School of Technology</a>, All Rights Reserved
+					</center></font></text>		
+		</body>
+</html>
